f7722338feba9143ff31bcaf6074d314561cb882
foisal404 <foisalahmmed2@gmail.com>
2024-06-23 00:16:59 +0600

    banner image change

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -2,26 +2,34 @@ import React from "react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
-import bannerImgFirst from "../../../assets/banner/Banner01.jpg";
-import bannerImgSecond from "../../../assets/banner/Banner02.jpg";
-import bannerImgThird from "../../../assets/banner/Banner03.jpg";
-import bannerImgForth from "../../../assets/banner/Banner04.jpg";
+import bannerImgI from "../../../assets/banner/Banner01.jpg";
+import bannerImgII from "../../../assets/banner/Banner02.jpg";
+import bannerImgIII from "../../../assets/banner/Banner03.jpg";
+import bannerImgIV from "../../../assets/banner/Banner04.jpg";
+import bannerImgV from "../../../assets/banner/Banner05.jpg";
+import bannerImgVI from "../../../assets/banner/Banner06.jpg";
 
 const Banner = () => {
   return (
     <div className="z-0">
       <Carousel showArrows={false} autoPlay showStatus={false}>
         <div>
-          <img src={bannerImgFirst} className="max-h-[35rem]" />
+          <img src={bannerImgVI} className="max-h-[35rem]" />
         </div>
         <div>
-          <img src={bannerImgSecond} className="max-h-[35rem]" />
+          <img src={bannerImgI} className="max-h-[35rem]" />
         </div>
         <div>
-          <img src={bannerImgThird} className="max-h-[35rem]" />
+          <img src={bannerImgII} className="max-h-[35rem]" />
         </div>
         <div>
-          <img src={bannerImgForth} className="max-h-[35rem]" />
+          <img src={bannerImgIII} className="max-h-[35rem]" />
+        </div>
+        <div>
+          <img src={bannerImgIV} className="max-h-[35rem]" />
+        </div>
+        <div>
+          <img src={bannerImgV} className="max-h-[35rem]" />
         </div>
       </Carousel>
     </div>
